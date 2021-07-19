ba66ea3fe652913486b3e9ccbfc047f0dce4bcf9
Daniel Farias <daniel.dsfarias@gmail.com>
2021-07-19 16:55:03 -0300

    Fix jest test

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import { writeTextSync } from "."
-
-const SLACK_URL: any = process.env.SLACK_URL;
-
-async function main(){
-    writeTextSync("test.txt", ['a', 'b'])
-}
-
-(main())
---
+++ tests/main.test.ts
@@ -0,0 +1,11 @@
+//import { writeTextSync } from "./../src"
+
+it('works', () => {
+    let value = 9
+    expect(value-6).toBe(3)
+})
+// test('adds 1 + 2 to equal 3', () => {
+//     writeTextSync("test.txt", ['a', 'b'])
+
+//     expect(1+2).toBe(3)
+// })
